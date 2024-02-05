6305f360fff1ba5126a18fb78f211751947cf4aa
Tilcerová Andrea <tilcerova@oksystem.cz>
2024-02-05 08:50:52 +0100

    oprava mobilniho menu

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -99,9 +99,9 @@ export function Navigation() {
                                 <div className="-my-6">
                                     <div className="space-y-2 py-6">
                                         {navigation.map((item) => (
-                                            <Link
-                                                to={item.name}
-                                                href={item.href}
+                                            <Link onClick={() => setMobileMenuOpen(false)}
+                                                key={item.name}
+                                                to={item.href}
                                                 className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800"
                                             >
                                                 {item.name}
